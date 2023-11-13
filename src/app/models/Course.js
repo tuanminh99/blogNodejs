1ddf22df237ed8539cfccf87149ce2335f6ce914
minh <minhcola9999@gmail.com>
2023-11-13 14:06:32 +0700

    course detail

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const Course = new Schema({
   name: { type: String, minLength: 5 },
   image: { type: String, minLength: 5 },
+  videoId: { type: String },
   des: { type: String, minLength: 5 },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
